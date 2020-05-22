78587306573e9f3ee70a7d91f9cb5ea2cbc7b777
Scott Hamilton <sgn.hamilton@protonmail.com>
2020-05-22 19:32:31 +0200

    Moved from scotthamilton to shamilton

--- modules/day-night-plasma-wallpapers.nix
+++ modules/day-night-plasma-wallpapers.nix
@@ -19,15 +19,15 @@ in {
 
     package = mkOption {
       type = types.package;
-      default = nur.repos.scotthamilton.day-night-plasma-wallpapers;
-      defaultText = "nur.repos.scotthamilton.day-night-plasma-wallpapers";
+      default = nur.repos.shamilton.day-night-plasma-wallpapers;
+      defaultText = "nur.repos.shamilton.day-night-plasma-wallpapers";
       description = "Day-night-plasma-wallpapers derivation to use.";
     };
 
     path = mkOption {
       type = types.listOf types.path;
       default = [];
-      example = literalExample "[ pkgs.bash nur.repos.scotthamilton.day-night-plasma-wallpapers ]";
+      example = literalExample "[ pkgs.bash nur.repos.shamilton.day-night-plasma-wallpapers ]";
       description = "List of derivations to put in path.";
     };
 
