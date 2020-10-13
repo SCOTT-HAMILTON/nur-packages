8ee1bd52c317ffccf7e0e93fdda88da7571837fe
SCOTT-HAMILTON <sgn.hamilton+github@protonmail.com>
2020-10-13 22:15:05 +0200

    pyrect: init at 0.1.4

--- default.nix
+++ default.nix
@@ -65,6 +65,9 @@ rec {
   python3-xlib = with pkgs.python3Packages; pkgs.callPackage ./pkgs/python3-xlib {
     inherit buildPythonPackage fetchPypi;
   };
+  pyrect = with pkgs.python3Packages; pkgs.callPackage ./pkgs/pyrect {
+    inherit buildPythonPackage fetchPypi;
+  };
   pyscreeze = with pkgs.python3Packages; pkgs.callPackage ./pkgs/pyscreeze {
     inherit buildPythonPackage fetchPypi pillow;
   };
