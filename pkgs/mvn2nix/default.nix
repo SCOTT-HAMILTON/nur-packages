9645a1331407aac7001527ea541d4c788ec8949e
SCOTT-HAMILTON <sgn.hamilton+github@protonmail.com>
2021-11-14 21:07:51 +0100

    mvn2Nix: commit bump

--- pkgs/mvn2nix/default.nix
+++ pkgs/mvn2nix/default.nix
@@ -5,7 +5,7 @@ import
   (fetchFromGitHub {
     owner = "fzakaria";
     repo = "mvn2nix";
-    rev = "3af3a7e02bce9cccc77c582a589d742e4fb8540e";
-    sha256 = "0v5jv129a8wg7hgpcjgkvi8kadmh4x8gm9vacb35jlwyvhf00kal";
+    rev = "ea21cfe97069feee55fa307ca9b125616c1fa84f";
+    sha256 = "0bs6j59i5c2lc7jnvifnvc5igp77cbhs78svkyia2gg4miifzwdz";
   }) {}
 
