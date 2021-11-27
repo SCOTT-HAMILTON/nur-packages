ff9292652a946c97e444511f59ba1f366d14f25e
SCOTT-HAMILTON <sgn.hamilton+github@protonmail.com>
2021-11-27 13:12:37 +0100

    sync-database: fix meta

--- pkgs/sync-database/default.nix
+++ pkgs/sync-database/default.nix
@@ -34,6 +34,7 @@ python3Packages.buildPythonPackage rec {
 
   meta = with lib; {
     description = "Keepass databases synching script to ssh servers and phone";
+    homepage = "https://github.com/SCOTT-HAMILTON/sync-database";
     license = licenses.mit;
     maintainers = [ "Scott Hamilton <sgn.hamilton+nixpkgs@protonmail.com>" ];
     platforms = platforms.linux;
