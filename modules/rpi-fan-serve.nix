a22a66637a55c82e7bc3974165b9ff9af4030871
SCOTT-HAMILTON <sgn.hamilton+github@protonmail.com>
2021-12-26 12:31:57 +0100

    modules/rpi-fan-serve: use new forceHostPath container bindMount option

--- modules/rpi-fan-serve.nix
+++ modules/rpi-fan-serve.nix
@@ -56,6 +56,7 @@ with lib; {
         };
         "${containerSocketsDir}" = {
           hostPath = hostSocketsDir;
+          forceHostPath = true;
           isReadOnly = false;
         };
       };
