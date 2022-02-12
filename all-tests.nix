01a529a4ab77909bc8b2c0269dac8f7041a613d6
SCOTT-HAMILTON <sgn.hamilton+github@protonmail.com>
2022-02-12 13:56:29 +0100

    all-tests: ignore broken and non-derivations

--- all-tests.nix
+++ all-tests.nix
@@ -2,5 +2,9 @@
 let
   lib = pkgs.lib;
   shamilton = import ./. args;
+  onlyUnbroken = attrs:
+    lib.filterAttrs (n: v: !(builtins.hasAttr "broken" v.meta && v.meta.broken )) attrs;
+  onlyDerivations = attrs:
+    lib.filterAttrs (n: v: lib.isDerivation v) attrs;
 in 
-lib.attrValues shamilton.tests
+lib.attrValues (onlyUnbroken (onlyDerivations shamilton.tests))
