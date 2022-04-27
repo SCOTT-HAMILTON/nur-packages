247ef1c3fcd5be570c45e30adfb9ae6514c4437c
SCOTT-HAMILTON <sgn.hamilton+github@protonmail.com>
2022-04-27 20:56:08 +0200

    qrup: update

--- pkgs/qrup/default.nix
+++ pkgs/qrup/default.nix
@@ -10,16 +10,16 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "SCOTT-HAMILTON";
     repo = "qrup";
-    rev = "60aa628d20b62978ce90779c756f24a82f57f97d";
-    sha256 = "06qqj78354jw5pqwb2c21phn5qrrvzws61idyi46x0aij2w0g3i8";
+    rev = "c296a61c90dddacceb2c5019c0016c9083eee0fb";
+    sha256 = "0frw7nnhyizljbwmcylqlxy6ghcxx83h88x036qcnwjjd0yjghg2";
   };
 
-  cargoSha256 = "0r0ann1fcqdj07s9anxqpfcl3n99ll54frmzw9ydqqknp7sw1df4";
+  cargoSha256 = "0ig2vl5xg40jjzvc4glmxhl9j7fryhyc9ikj0qz6w4vag39lj8i2";
   verifyCargoDeps = true;
 
   meta = with lib; {
-    description = "Check if tables and items in a .toml file are lexically sorted";
-    homepage = "https://github.com/devinr528/cargo-sort-ck";
+    description = "Transfer files over LAN from your mobile device to your computer.";
+    homepage = "https://github.com/SCOTT-HAMILTON/qrup";
     license = licenses.mit;
     maintainers = [ "Scott Hamilton <sgn.hamilton+nixpkgs@protonmail.com>" ];
     platforms = platforms.linux;
