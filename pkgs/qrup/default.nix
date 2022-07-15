b376b61db51dc2c493a7d85e6cc999e78b3b0a43
SCOTT-HAMILTON <sgn.hamilton+github@protonmail.com>
2022-07-15 12:37:04 +0200

    qrup: rev bump

--- pkgs/qrup/default.nix
+++ pkgs/qrup/default.nix
@@ -5,16 +5,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "qrup";
-  version = "unstable";
+  version = "2022-06-17";
 
   src = fetchFromGitHub {
     owner = "SCOTT-HAMILTON";
     repo = "qrup";
-    rev = "c296a61c90dddacceb2c5019c0016c9083eee0fb";
-    sha256 = "0frw7nnhyizljbwmcylqlxy6ghcxx83h88x036qcnwjjd0yjghg2";
+    rev = "8c4fd1e7057f7d9d11ad814942f6090c59c267bd";
+    sha256 = "sha256-DQWJ+sHO3BWEr/3JzUpfcIPVwUP44erARPqHMJMlp1k=";
   };
 
-  cargoSha256 = "0ig2vl5xg40jjzvc4glmxhl9j7fryhyc9ikj0qz6w4vag39lj8i2";
+  cargoSha256 = "sha256-buY+MsoQ+G6/KoSWGdWctSNiQy1YXdwaFZzghVtGKi0=";
   verifyCargoDeps = true;
 
   meta = with lib; {
