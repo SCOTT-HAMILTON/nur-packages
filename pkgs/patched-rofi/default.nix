3ece0e906699dd8e20e83af646a54bb7b9fbadfa
SCOTT-HAMILTON <sgn.hamilton+github@protonmail.com>
2022-10-30 23:12:29 +0100

    patched-rofi: fix patches

--- pkgs/patched-rofi/default.nix
+++ pkgs/patched-rofi/default.nix
@@ -2,7 +2,7 @@
 rofi-unwrapped.overrideAttrs (old: {
   patches = (old.patches or []) ++ [
     (
-      if nixosVersion == "nixpkgs-unstable" then ./add-nix-dirs-app-desktop-search.patch else ./add-nix-dirs-app-desktop-search-old.patch
+      if nixosVersion != "master" then ./add-nix-dirs-app-desktop-search.patch else ./add-nix-dirs-app-desktop-search-old.patch
     )
   ];
 })
