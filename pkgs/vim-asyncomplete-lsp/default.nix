ee6c75b7aac3f68650ae2529c67de2c638b5f6f3
SCOTT-HAMILTON <sgn.hamilton+github@protonmail.com>
2022-12-20 19:37:24 +0100

    vim-asyncomplete-lsp: 2021-12-17 -> 2022-11-21

--- pkgs/vim-asyncomplete-lsp/default.nix
+++ pkgs/vim-asyncomplete-lsp/default.nix
@@ -6,13 +6,13 @@
 
 buildVimPluginFrom2Nix {
   pname = "vim-asyncomplete-lsp";
-  version = "2021-12-17";
+  version = "2022-11-21";
 
   src = fetchFromGitHub {
     owner = "prabirshrestha";
     repo = "asyncomplete-lsp.vim";
-    rev = "f6d6a6354ff279ba707c20292aef0dfaadc436a3";
-    sha256 = "1y0wpq982nw0ibqhvcvb7md58jvadygkxc1ibg99zxw1kznfpla6";
+    rev = "cc5247bc268fb2c79d8b127bd772514554efb3ee";
+    sha256 = "sha256-SeEAy/jtrdHerZPVjQZXANTcuvMndIIWgGh3B8Ik1NM=";
   };
 
   meta = with lib; {
