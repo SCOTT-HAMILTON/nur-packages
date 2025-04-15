b7ab020c622bf4ab35e925411a78b4c6bbd3a21a
SCOTT-HAMILTON <sgn.hamilton+github@protonmail.com>
2025-04-15 16:19:41 +0200

    L-ve3D: Update

--- pkgs/L-ove3D/default.nix
+++ pkgs/L-ove3D/default.nix
@@ -1,23 +1,24 @@
 { stdenv
 , lib
+, fetchFromGitHub
 , makeWrapper
-, nix-gitignore
-# , fetchFromGitHub
+# , nix-gitignore
 , love
 }:
 stdenv.mkDerivation rec {
   pname = "L-ve3D";
   version = "1.0";
 
-  # src = fetchFromGitHub {
-  #   owner = "p-ranav";
-  #   repo = "argparse";
-  #   rev = "v${version}";
-  #   sha256 = "sha256-0fgMy7Q9BiQ/C1tmhuNpQgad8yzaLYxh5f6Ps38f2mk=";
-  # };
-  src = nix-gitignore.gitignoreSource [ ] ~/GIT/L-ve3D;
+  src = fetchFromGitHub {
+    owner = "SCOTT-HAMILTON";
+    repo = "L-ve3D";
+    rev = "0e048e3b3206a6acbe9a0d2ca097882c43fc1f51";
+    sha256 = "sha256-I7kseSwHy6SLsBlPmHocyOcCJYMEBv1ieSTToz+XfdE=";
+  };
+  # src = nix-gitignore.gitignoreSource [ ] ~/GIT/L-ve3D;
 
   nativeBuildInputs = [ makeWrapper ];
+  propagatedBuildInputs = [ love ];
 
   installPhase = ''
     mkdir -p "$out/src/${pname}"
