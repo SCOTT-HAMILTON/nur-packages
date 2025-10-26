23ab79fcf1bec21d016db09a5b1a244ce961800f
SCOTT-HAMILTON <sgn.hamilton+github@protonmail.com>
2025-10-26 13:11:33 +0100

    flake update

--- flakes/master/flake.nix
+++ flakes/master/flake.nix
@@ -1,7 +1,7 @@
 {
   description = "My personal NUR repository";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/25.05";
   };
   outputs = { self, nixpkgs }:
     let
