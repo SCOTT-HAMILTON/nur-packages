6351cb338b728543d6ad29f6f4259ae236c2412e
SCOTT-HAMILTON <sgn.hamilton+github@protonmail.com>
2025-10-26 13:01:31 +0100

    lib gomod2nix

--- lib/default.nix
+++ lib/default.nix
@@ -1,8 +1,14 @@
 { pkgs }:
 
-with pkgs.lib; {
+with pkgs.lib; let
+  builder0 = pkgs.callPackage ./gomod2nix-no-cyclic-deps/builder { };
+  gomod2nix0 = pkgs.callPackage ./gomod2nix-no-cyclic-deps { inherit (builder0) buildGoApplication; };
+  builder1 = pkgs.callPackage ./gomod2nix/builder { gomod2nix = gomod2nix0; };
+  # gomod2nix = pkgs.callPackage ./gomod2nix { inherit buildGoApplication; };
+in {
   # Add your library functions here
   #
   # hexint = x: hexvals.${toLower x};
+  gomod2nix = { inherit (builder1)  buildGoApplication; };
 }
 
